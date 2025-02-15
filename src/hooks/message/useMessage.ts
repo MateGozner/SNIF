2b9891e784e44fd2012f2e16bd33ddb0b9e7b794
Gozner Máté <mate.gozner@stud.ubbcluj.ro>
2025-02-15 20:08:30 +0200

    Refactor message hooks: update API paths for fetching messages and user chats, and remove unused mark message as read functionality.

--- src/hooks/message/useMessage.ts
+++ src/hooks/message/useMessage.ts
@@ -1,31 +1,18 @@
 import { api } from "@/lib/auth/api";
 import { ChatSummaryDto, MessageDto } from "@/lib/types/message";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 
 export function useMatchMessages(matchId: string) {
   return useQuery({
     queryKey: ["messages", matchId],
-    queryFn: () => api.get<MessageDto[]>(`api/chat/${matchId}`),
+    queryFn: () =>
+      api.get<MessageDto[]>(`api/chats/matches/${matchId}/messages`),
   });
 }
 
 export function useUserChats(userId: string) {
   return useQuery({
     queryKey: ["chats", userId],
-    queryFn: () => api.get<ChatSummaryDto[]>(`api/chat/${userId}/chats`),
-  });
-}
-
-export function useMarkMessageAsRead() {
-  const queryClient = useQueryClient();
-
-  return useMutation({
-    mutationFn: (messageId: string) =>
-      api.post(`api/chat/${messageId}/read`, {}),
-    onSuccess: (_, messageId) => {
-      queryClient.invalidateQueries({
-        queryKey: ["messages", messageId],
-      });
-    },
+    queryFn: () => api.get<ChatSummaryDto[]>(`api/chats/users/${userId}`),
   });
 }
